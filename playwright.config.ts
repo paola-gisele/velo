3e363036d809ca2c8ad64e4b0ddc0f38ceda7b4f
Gisele Paola de Souza <gisele.souza.t_cit@grupoboticario.com.br>
2026-05-07 09:50:59 -0300

    feat: adicionando o deploy preview via github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://velo-one-cyan.vercel.app",
+    // baseURL: "http://localhost:5173",
+    baseURL: "https://velo-paola-gisele-paola-giseles-projects.vercel.app",
+    // baseURL: "https://velo-one-cyan.vercel.app",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on",
