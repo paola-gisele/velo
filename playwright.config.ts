f06e4b049b2b84ce4754586d31ab1fc2ef11059f
Gisele Paola de Souza <gisele.souza.t_cit@grupoboticario.com.br>
2026-05-07 11:44:50 -0300

    feat: adição dos testes e2e em preview

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,8 +34,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: "http://localhost:5173",
-    baseURL: "https://velo-paola-gisele-paola-giseles-projects.vercel.app",
+    baseURL: process.env.BASE_URL || "http://localhost:5173",
+    // baseURL: "https://velo-paola-gisele-paola-giseles-projects.vercel.app",
     // baseURL: "https://velo-one-cyan.vercel.app",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
